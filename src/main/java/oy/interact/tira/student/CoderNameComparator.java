1e3b4c94ea69ee6d1d26aa47afdc69e275459c40
tmarttil22 <marttilatino@gmail.com>
2023-09-29 19:15:51 +0300

    test 02

---
+++ src/main/java/oy/interact/tira/student/CoderNameComparator.java
@@ -0,0 +1,13 @@
+package oy.interact.tira.student;
+
+import java.util.Comparator;
+
+public class CoderNameComparator implements Comparator<CoderNameComparator>{
+
+    @Override
+    public int compare(CoderNameComparator o1, CoderNameComparator o2) {
+      int x = 1;
+      return x;
+    }
+
+}
